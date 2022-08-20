2dacfccd6401080e5e1f922469600d06193db6b7
Harshana369 <harsha36998@gmail.com>
2022-08-20 22:58:24 +0530

    mogodb connect

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 import express from "express";
 import products from "./data/Products.js";
+import dotenv from "dotenv";
+import connectDatabase from "./config/MongoDb.js";
+
+dotenv.config();
+connectDatabase();
 const app = express();
 
 // LODE PRODUCTS FROM SERVER
 app.get("/api/products", (req, res) => {
+  console.log("harshana");
   res.json(products);
 });
 
 // SINGLE PRODUCTS FROM SERVER
 app.get("/api/products/:id", (req, res) => {
+  console.log(req.params.id);
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
@@ -17,4 +24,6 @@ app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
-app.listen(5000, console.log("server running port 5000..."));
+const PORT = process.env.PORT || 1000;
+
+app.listen(PORT, console.log(`server run in port ${PORT}`));
