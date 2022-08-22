9eec051ba515946f7bcd035aecf8a11f15f546e8
Harshana369 <harsha36998@gmail.com>
2022-08-22 09:28:01 +0530

    creact route and insert data to databases

--- server/server.js
+++ server/server.js
@@ -2,23 +2,17 @@ import express from "express";
 import products from "./data/Products.js";
 import dotenv from "dotenv";
 import connectDatabase from "./config/MongoDb.js";
+import ImportData from "./DataImport.js";
 
 dotenv.config();
 connectDatabase();
 const app = express();
 
-// LODE PRODUCTS FROM SERVER
-app.get("/api/products", (req, res) => {
-  console.log("harshana");
-  res.json(products);
-});
 
-// SINGLE PRODUCTS FROM SERVER
-app.get("/api/products/:id", (req, res) => {
-  console.log(req.params.id);
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+//API
+app.use("/api/import", ImportData);
+
+
 
 app.get("/", (req, res) => {
   res.send("API is running....");
