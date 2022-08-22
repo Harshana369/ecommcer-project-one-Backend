1417fc2d3dea6cd4680a180b9d46f2807ff56523
Harshana369 <harsha36998@gmail.com>
2022-08-22 10:48:29 +0530

    error handler make

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDatabase from "./config/MongoDb.js";
 import ImportData from "./DataImport.js";
 import productsRoute from "./Routes/ProductsRoutes.js";
+import { errorHandler, notFound } from "./Middleware/Errors.js";
 
 dotenv.config();
 connectDatabase();
@@ -12,9 +13,9 @@ const app = express();
 app.use("/api/import", ImportData);
 app.use("/api/products", productsRoute);
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
+// ERROR HANDLER
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 1000;
 
