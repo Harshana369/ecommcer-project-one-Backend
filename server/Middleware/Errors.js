8b74a3b7fe593f7f2c1ff032f0a0a23d5453ad99
Harshana369 <harsha36998@gmail.com>
2022-08-22 14:07:25 +0530

    learn middleware and error hadding

--- server/Middleware/Errors.js
+++ server/Middleware/Errors.js
@@ -1,11 +1,21 @@
+
 const notFound = (req, res, next) => {
   const error = new Error(`Not found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
+//The error that comes when the url is wrong (Client)
+// pass karana url id waradi unahama(500 -> clint api end point eka hariyata dunne na -> 404)
+// ObjectId eka waradiyata dunnahama(200 -> clint send karana id adala deta ekak hoyaganna ba -> 500)
 const errorHandler = (err, req, res, next) => {
+    console.log("open");
+
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  console.log("start");
+  console.log(res.statusCode === 200);
+  console.log(res.statusCode);
+
   res.status(statusCode);
   res.json({
     message: err.message,
