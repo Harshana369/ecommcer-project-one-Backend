4572097a736fd32c962251a752f3ee1f09c294c2
Harshana369 <harsha36998@gmail.com>
2022-08-24 07:52:40 +0530

    creact authorization user and register user

--- server/Middleware/AuthMiddleware.js
+++ server/Middleware/AuthMiddleware.js
@@ -13,7 +13,6 @@ const protect = asyncHandler(async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
 
       const decoded = Jwt.verify(token, process.env.JWT_SECRET);
-      console.log(decoded);
       req.user = await User.findById(decoded.id).select("-password");
       next();
     } catch (error) {
