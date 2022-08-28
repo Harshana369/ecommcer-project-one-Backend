e9e5880d9ec65caf2242c2a13962f2748cbd53cf
Harshana369 <harsha36998@gmail.com>
2022-08-28 23:48:30 +0530

    set paypal route

--- server/server.js
+++ server/server.js
@@ -16,6 +16,9 @@ app.use("/api/import", ImportData);
 app.use("/api/products", productsRoute);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
 
 
 // ERROR HANDLER
