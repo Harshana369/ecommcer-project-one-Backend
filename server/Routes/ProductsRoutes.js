168a9c360c413fe4fbaf4b48b690a595fb55a7e6
Harshana369 <harsha36998@gmail.com>
2022-08-29 22:44:14 +0530

    create product searching

--- server/Routes/ProductsRoutes.js
+++ server/Routes/ProductsRoutes.js
@@ -1,17 +1,23 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import Product from "../Models/ProductModel.js";
-import protect from "./../Middleware/AuthMiddleware.js"
-
+import protect from "./../Middleware/AuthMiddleware.js";
 
 const productsRoute = express.Router();
-
 
 //GET ALL PRODUCTS
 productsRoute.get(
   "/",
-  asyncHandler(async (req,res) => {
-    const products = await Product.find({});
+  asyncHandler(async (req, res) => {
+    const keyword = req.query.keyword
+      ? {
+          name: {
+            $regex: req.query.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+    const products = await Product.find({ ...keyword });
 
     res.json(products);
   })
@@ -22,15 +28,14 @@ productsRoute.get(
   "/:id",
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
-        if (product) {
-            res.json(product);
-        }else{
-            res.status(404);
-            throw new Error("Product not Found")
-        }
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not Found");
+    }
   })
 );
-
 
 // PRODUCT REVIEW
 productsRoute.post(
